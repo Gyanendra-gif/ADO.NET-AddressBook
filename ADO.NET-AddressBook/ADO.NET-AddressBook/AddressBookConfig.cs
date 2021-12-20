f2a1c2079666ecafa8c82b4630749730a83add51
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-12-20 11:55:04 +0530

    [Gyanendra] Add: A Program to Add Contacts in Address Book

---
+++ ADO.NET-AddressBook/ADO.NET-AddressBook/AddressBookConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ADO.NET_AddressBook
+{
+    class AddressBookConfig
+    {
+        private SqlConnection con;
+        private void Connection()
+        {
+            string connectionString = "Server = (localdb)\\MSSQLLocalDB; Database = address_book; Trusted_Connection = true";
+            con = new SqlConnection(connectionString);
+        }
+        public Contact AddContact(Contact contact) 
+        {
+            try
+            {
+                Connection();
+                SqlCommand com = new SqlCommand("AddContact", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@FirstName", contact.FirstName);
+                com.Parameters.AddWithValue("@LastName", contact.LastName);
+                com.Parameters.AddWithValue("@Address", contact.Address);
+                com.Parameters.AddWithValue("@City", contact.City);
+                com.Parameters.AddWithValue("@State", contact.State);
+                com.Parameters.AddWithValue("@ZipCode", contact.ZipCode);
+                com.Parameters.AddWithValue("@Phone", contact.Phone);
+                com.Parameters.AddWithValue("@Email", contact.Email);
+                con.Open();
+                int i = com.ExecuteNonQuery();
+                con.Close();
+                if (i >= 1)
+                    return contact;
+                else
+                    return null;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }            
+        }
+    }
+}
---
+++ ADO.NET-AddressBook/ADO.NET-AddressBook/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ADO.NET_AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ADO.NET Address Book");
+            AddressBookConfig addressBookConfig = new AddressBookConfig();
+            Contact contact = new Contact();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter your Choice Number to Execute the Program Press- 1-Addition of Contact, 2-Deletion of Contact, 3-Updation of Contact, 4-View, 5-Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        contact.FirstName = "Ram";
+                        contact.LastName = "Singh";
+                        contact.Address = "Rewa";
+                        contact.City = "Satna";
+                        contact.State = "M.P";
+                        contact.ZipCode = 123456;
+                        contact.Phone = 1234567890;
+                        contact.Email = "xyz@gmail.com";
+                        var result = addressBookConfig.AddContact(contact);
+                        if (result != null)                        
+                            Console.WriteLine("Data of Customer Added Successfully");
+                        else
+                            Console.WriteLine("Provide Data According to Coloumn");                        
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
